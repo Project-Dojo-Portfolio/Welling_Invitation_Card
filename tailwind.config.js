113a87d5aa1ecc7d3efa9e07a5452708df34941a
Kevin Andres Ortiz Merchan - vn559a9 <kevinandresortizmerchan@outlook.com>
2023-06-30 13:32:53 -0500

    setup project

---
+++ tailwind.config.js
@@ -0,0 +1,38 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./public/index.html", "./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      backgroundImage: {
+        'asistir':"url('../img/asistir.jpeg')",
+        'collage1':"url('../img/collage1.jpeg')",
+        'collage2':"url('../img/collage2.jpeg')",
+        'collage3':"url('../img/collage3.jpeg')",
+        'collage4':"url('../img/collage4.jpeg')",
+        'collage5':"url('../img/collage5.jpeg')",
+        'collage6':"url('../img/collage6.jpeg')",
+        'collage7':"url('../img/collage7.jpeg')",
+        'collage8':"url('../img/collage8.jpeg')",
+        'our_story':"url('../img/our_story.png')",
+        'portada':"url('../img/portada.jpeg')",
+        'collage9':"url('../img/collage9.jpeg')"
+      },
+      backgroundColor: theme => ({
+        ...theme('colors'),
+        'primary': '#FEFEFE',
+        'secundary': '#F3F0ED',
+        'terciary': '#90887F',
+        'extra':'#55524B'
+      }),
+      textColor: theme => ({
+        ...theme('colors'),
+        'primary': '#FEFEFE',
+        'secundary': '#F3F0ED',
+        'terciary': '#90887F',
+        'extra':'#55524B'
+      }),
+    },
+  },
+  plugins: [],
+}
+
